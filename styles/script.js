3a49d7f16c761b797473e0d4e948e2b8c576a3d9
Abiodun ogundiran <ogundiranabbey057@gmail.com>
2024-02-29 12:18:20 +0100

    firdt

--- styles/script.js
+++ styles/script.js
@@ -1,5 +1,5 @@
  // Set the date we're counting down to
- const giveawayDate = new Date('2024-03-07').getTime();
+ const giveawayDate = new Date('2024-03-08').getTime('11:30');
 
  // Update the countdown every 1 second
  const countdown = setInterval(() => {
@@ -16,8 +16,11 @@
    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
    
    // Display the result in the element with id="countdown"
-   document.getElementById('countdown').innerHTML = `Giveaway ends in: ${days}d ${hours}h ${minutes}m ${seconds}s`;
-   
+  //  document.getElementById('countdown').innerHTML = `Giveaway ends in: ${days}d ${hours}h ${minutes}m ${seconds}s`;
+   document.getElementById('days').innerHTML = `${days}`;
+   document.getElementById('hours').innerHTML = `${hours}`;
+   document.getElementById('minutes').innerHTML = `${minutes}`;
+   document.getElementById('seconds').innerHTML = `${seconds}`;
    // If the countdown is over, display a message
    if (distance < 100000000) {
      clearInterval(countdown);
